16fed16525be1bd5d1e7d1530c4b6a15dcd27d28
HerrBerglund <35253558+HerrBerglund@users.noreply.github.com>
2018-09-26 10:22:48 +0200

    added checkbox

--- mainjs.js
+++ mainjs.js
@@ -1,4 +1,3 @@
-
 let toggleAll = document.querySelector("#btnToggleAll");
 
 textBoxEventListener();
@@ -7,27 +6,26 @@ textBoxEventListener();
 
 // function adds eventlistener to the textbox enter keypress event
 
-function textBoxEventListener(){
+function textBoxEventListener() {
     let textBox = document.querySelector("#newTodoTextbox");
 
-    textBox.addEventListener("keypress", event =>{
+    textBox.addEventListener("keypress", event => {
         var key = event.which || event.keyCode;
-        if (key == 13){
+        if (key == 13) {
             let ulExists = document.querySelector("#todoApp-extension ul");
-            if(ulExists == null){
+            if (ulExists == null) {
                 createList();
                 addToList();
-            }
-            else{
+            } else {
                 addToList();
             }
         }
-        });
-        
+    });
+
 }
 
 // creates ul element
-function createList(){
+function createList() {
     let parent = document.querySelector("#todoApp-extension");
     let ul = document.createElement("ul");
     parent.appendChild(ul);
@@ -35,23 +33,27 @@ function createList(){
     appFooter.style.display = "block";
 }
 // creates li element
-function addToList(){
+function addToList() {
     let parent = document.querySelector("#todoApp-extension ul");
     let li = document.createElement("li");
     let div1 = document.createElement("div");
-    let checkbox = document.createElement("checkbox");
+    let checkbox = document.createElement('input');
     let label = document.createElement("label");
     let button = document.createElement("button");
     let textbox = document.querySelector("#newTodoTextbox");
+    checkbox.type="checkbox";
+    checkbox.value="value";
+    
+    
     label.textContent = textbox.value;
     li.className = "listItem";
-    
+
 
     li.appendChild(div1);
     div1.appendChild(checkbox);
     div1.appendChild(label);
     div1.appendChild(button);
-    
+
     parent.appendChild(li);
     textbox.value = "";
 }
